041ab8cebb1c89db90af9c4830306e874d54c64e
pzhao123 <peterzhao080@gmail.com>
2021-11-19 11:40:47 -0500

    update

--- shell.h
+++ shell.h
@@ -7,3 +7,9 @@
 #include <sys/stat.h>
 #include <time.h>
 #include <dirent.h>
+
+//parse args
+
+//command num
+
+//command separator
--- test.c
+++ test.c
@@ -7,6 +7,7 @@
 #include <sys/stat.h>
 #include <time.h>
 #include <dirent.h>
+#include <shell.h>
 #define INPUT_SIZE 256
 
 
@@ -20,7 +21,15 @@ int main(){
         fgets(input, sizeof(input), stdin);
         sscanf(input, "%s", input);
         char ** args = parse_args(input);
-        
+        if (!args[0]) {
+            if (!strcmp(args[0], "exit")) {
+                running = 0;
+                break;
+            }
+            else if (!strcmp(args[0], "cd")) {
+                chdir(args[0]);
+            }
+        }
         //run parse args
         //if cd or exit
         //run exec
